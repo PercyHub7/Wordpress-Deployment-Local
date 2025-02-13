da2d3d50fe857305f97360102cc56d1f3aefcb49
percyhub7 <iuchenna28@gmail.com>
2025-02-13 15:19:46 +0100

    Add documentation for wordpress deployment using Docker

--- wp-config.php
+++ wp-config.php
@@ -44,10 +44,10 @@ if (!function_exists('getenv_docker')) {
 define( 'DB_NAME', getenv_docker('WORDPRESS_DB_NAME', 'wordpress') );
 
 /** Database username */
-define( 'DB_USER', getenv_docker('WORDPRESS_DB_USER', 'wp_user') );
+define( 'DB_USER', getenv_docker('WORDPRESS_DB_USER', 'example username') );
 
 /** Database password */
-define( 'DB_PASSWORD', getenv_docker('WORDPRESS_DB_PASSWORD', '$P$B.efrVOgFtBvKyB05nrep5MMQUJghB/') );
+define( 'DB_PASSWORD', getenv_docker('WORDPRESS_DB_PASSWORD', 'example password') );
 
 /**
  * Docker image fallback values above are sourced from the official WordPress installation wizard:
